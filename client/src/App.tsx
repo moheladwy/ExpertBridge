c87e7fe4d5ea03d741b84da2198bb33ad56cb4e4
moheladwy <mohamed.h.eladwy@gmail.com>
2025-01-27 17:57:04 +0200

    Added the Initial Setup for the ReactTS Client Project, Added the Initial Setup for Dockerfiles and docker-compose files, and Added github linting actions for typescript and dotnet (C#).

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <img src={reactLogo} alt="React Logo" style={{ height: '1em', margin: "1rem" }} />
+      <h1 style={{ height: '1em', margin: "1rem" }} >Expert Bridge (تبادل الخبرات)</h1>
+      <img src={viteLogo} alt="Vite Logo" style={{ height: '1em', margin: "1rem" }} />
+    </>
+  )
+}
+
+export default App
