000398e4fa697966557df6b4671a3946b4c71eb0
Kilany <y.m.elkilany@gmail.com>
2025-03-20 23:17:33 +0200

    [api/file-structure] Apply some modifications to the file structure

---
+++ server/ExpertBridge.Api/Application/Services/FirebaseAuthService.cs
@@ -0,0 +1,48 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using ExpertBridge.Api.Configurations;
+using ExpertBridge.Api.Core.Interfaces.Services;
+using FirebaseAdmin.Auth;
+
+namespace ExpertBridge.Api.Application.Services;
+
+public class FirebaseAuthService(HttpClient httpClient) : IFirebaseAuthService
+{
+    private readonly FirebaseAuth _auth = FirebaseAuth.DefaultInstance;
+
+    public async Task<string> RegisterAsync(string email, string password)
+    {
+        var userArgs = new UserRecordArgs
+        {
+            Email = email,
+            Password = password,
+            Disabled = false,
+            DisplayName = email,
+            EmailVerified = true
+        };
+
+        var userRecord = await _auth.CreateUserAsync(userArgs);
+        return userRecord.Uid;
+    }
+
+    public async Task<string> LoginAsync(string email, string password)
+    {
+        var request = new { email, password, returnSecureToken = true };
+        var response = await httpClient.PostAsJsonAsync("", request);
+        var authToken = await response.Content.ReadFromJsonAsync<AuthToken>();
+        return authToken.IdToken;
+    }
+
+    public async Task<FirebaseToken?> VerifyIdTokenAsync(string idToken)
+    {
+        try
+        {
+            return await _auth.VerifyIdTokenAsync(idToken, true);
+        }
+        catch
+        {
+            return null; // Invalid session
+        }
+    }
+}
---
+++ server/ExpertBridge.Api/Application/Services/PostsService.cs
@@ -0,0 +1,25 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using ExpertBridge.Api.Core;
+using ExpertBridge.Api.Core.DTOs.Requests.CreatePost;
+using ExpertBridge.Api.Core.DTOs.Responses;
+using ExpertBridge.Api.Core.Entities.Posts;
+using ExpertBridge.Api.Core.Interfaces.Repositories;
+using ExpertBridge.Api.Core.Interfaces.Services;
+using FluentValidation;
+
+namespace ExpertBridge.Api.Application.Services;
+
+public class PostsService(
+    IEntityRepository<Post> postRepository,
+    IValidator<CreatePostRequest> createPostRequestValidator)
+    : IPostsService
+{
+    public async Task<PostResponse> GetByIdAsync(string id)
+    {
+        var post = await postRepository.GetByIdAsNoTrackingAsync(id);
+        if (post is null) throw new PostNotFoundException("Post not found");
+        return new PostResponse(post);
+    }
+}
