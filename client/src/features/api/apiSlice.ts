0f4b952f2840ae13783ffa9e5a3c2cee3ea90f38
Kilany <y.m.elkilany@gmail.com>
2025-03-22 01:24:25 +0200

    [client/config] Add the new Hostinger hosted api base url

--- client/src/features/api/apiSlice.ts
+++ client/src/features/api/apiSlice.ts
@@ -7,10 +7,11 @@ export const emptyApiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
     // baseUrl: config.API_HTTPS_BASE_URL,
-    // baseUrl: 'http://13.50.56.110:8080/api/posts', // Adawy's Server 
+    // baseUrl: 'http://13.50.56.110:8080/api/', // 
     // baseUrl: 'http://localhost:3500', // json-server
     // baseUrl: 'http://69.62.106.202:8080/api', // Hostinger
-    baseUrl: 'http://localhost:5027/api',
+    // baseUrl: 'http://localhost:5027/api',
+    baseUrl: 'https://api.expertbridge.duckdns.org/api',
 
     prepareHeaders: async (headers) => {
       const token = await auth.currentUser?.getIdToken();
