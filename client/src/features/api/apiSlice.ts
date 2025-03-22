19aaf17f1911ce91abc6417eb2ae95bcdac108be
Kilany <y.m.elkilany@gmail.com>
2025-03-22 23:32:27 +0200

    [client/config] Base Url is now based on the conifg VITE_SERVER_URL

--- client/src/features/api/apiSlice.ts
+++ client/src/features/api/apiSlice.ts
@@ -6,11 +6,11 @@ import { auth } from '@/lib/firebase';
 export const emptyApiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    // baseUrl: config.VITE_SERVER_URL,
+    baseUrl: config.VITE_SERVER_URL,
     // baseUrl: 'http://13.50.56.110:8080/api/', // 
     // baseUrl: 'http://localhost:3500', // json-server
     // baseUrl: 'http://69.62.106.202:8080/api', // Hostinger
-    baseUrl: 'http://localhost:5027/api',
+    // baseUrl: 'http://localhost:5027/api',
     // baseUrl: 'https://api.expertbridge.duckdns.org/api',
 
     prepareHeaders: async (headers) => {
