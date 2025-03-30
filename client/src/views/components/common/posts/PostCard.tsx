5d3d2bdd7dcdeb53c170bbe13eaad0a7c7b77bb6
abwael <11410120210490@stud.cu.edu.eg>
2025-03-30 06:33:37 +0200

    Copping the post link

--- client/src/views/components/common/posts/PostCard.tsx
+++ client/src/views/components/common/posts/PostCard.tsx
@@ -4,6 +4,17 @@ import { Link } from "react-router-dom";
 import { Post } from "@/features/posts/types";
 import { ArrowBigUp } from 'lucide-react';
 import { MessageCircle } from 'lucide-react';
+import { Ellipsis } from 'lucide-react';
+import { Link2 } from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/views/components/custom/dropdown-menu"
+import toast from "react-hot-toast";
 
 interface PostCardProps {
   post: Post;
@@ -22,29 +33,40 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
     console.log("Down")
   };
 
+  const handleCopyLink = () => {
+    const postUrl = `${window.location.origin}/feed/${post.id}`;
+    navigator.clipboard.writeText(postUrl)
+    .then(() => {
+      toast.success("Link copied successfully");
+    })
+    .catch((err) => {
+      toast.error("Failed to copy link");
+    });
+  }
+
   const voteDifference = post.upvotes - post.downvotes;
   const totalCommentsNumber = post.comments.length;
 
   return (
     <div className="flex flex-col gap-3 bg-white shadow-md rounded-lg p-4 border border-gray-200">
-      {/* Author Info */}
-      <div className="flex items-center space-x-3">
-        <img
-          src={post.author.profilePictureUrl}
-          // alt={`${post.author.id} Profile`}
-          width={40}
-          height={40}
-          className="rounded-full"
-        />
-        <div>
-          {/* Name */}
-          <h3 className="text-md font-semibold">{post.author.firstName + ' ' + post.author.lastName}</h3>
-          {/* Publish Date */}
-          <div className="flex justify-between items-center text-sm text-gray-500">
-            <span>{new Date(post.createdAt).toLocaleDateString()}</span>
+        {/* Author Info */}
+        <div className="flex items-center space-x-3">
+          <img
+            src={post.author.profilePictureUrl}
+            // alt={`${post.author.id} Profile`}
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
+          <div>
+            {/* Name */}
+            <h3 className="text-md font-semibold">{post.author.firstName + ' ' + post.author.lastName}</h3>
+            {/* Publish Date */}
+            <div className="flex justify-between items-center text-sm text-gray-500">
+              <span>{new Date(post.createdAt).toLocaleDateString()}</span>
+            </div>
           </div>
         </div>
-      </div>
 
       {/* Post Title */}
       <Link to={`/feed/${post.id}`}>
@@ -68,37 +90,54 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
       )}
 
       {/* Interactions */}
-      {/* Votes */}
-      <div className="flex gap-2 items-center">
-        <div className="flex gap-2 items-stretch  bg-gray-200 rounded-full w-fit">
-          <div className="rounded-l-full p-1 hover:bg-green-100  hover:cursor-pointer" onClick={handleUpVote}>
-            <ArrowBigUp className="text-gray-500 hover:text-green-400"/>
-          </div>
-          
-          <div className={`flex justify-center items-center text-sm font-bold ${voteDifference >= 0 ? "text-green-600" : "text-red-600"}`}>
-            {voteDifference}
-          </div>
+      <div className="flex justify-between items-center">
+        <div className="flex gap-2 items-center">
+          {/* Votes */}
+          <div className="flex gap-2 items-stretch  bg-gray-200 rounded-full w-fit">
+            <div className="rounded-l-full p-1 hover:bg-green-100  hover:cursor-pointer" onClick={handleUpVote}>
+              <ArrowBigUp className="text-gray-500 hover:text-green-400"/>
+            </div>
+            
+            <div className={`flex justify-center items-center text-sm font-bold ${voteDifference >= 0 ? "text-green-600" : "text-red-600"}`}>
+              {voteDifference}
+            </div>
 
-          <div className="rounded-l-full p-1 rotate-180 hover:bg-red-100  hover:cursor-pointer" onClick={handleDownVote}>
-            <ArrowBigUp className="text-gray-500 hover:text-red-400"/>
-          </div>
-        </div>
-
-        {/* Comments */}
-        <Link to={`/feed/${post.id}`}>
-          <div className="flex items-center gap-2 rounded-full p-1 hover:bg-gray-200  hover:cursor-pointer">
-            <MessageCircle className="text-gray-500"/>
-            <div className="text-gray-500 text-md font-bold ">
-              {totalCommentsNumber}
+            <div className="rounded-l-full p-1 rotate-180 hover:bg-red-100  hover:cursor-pointer" onClick={handleDownVote}>
+              <ArrowBigUp className="text-gray-500 hover:text-red-400"/>
             </div>
           </div>
-        </Link>
+
+          {/* Comments */}
+          <Link to={`/feed/${post.id}`}>
+            <div className="flex items-center gap-2 rounded-full p-1 px-2 hover:bg-gray-200  hover:cursor-pointer">
+              <MessageCircle className="text-gray-500"/>
+              <div className="text-gray-500 text-md font-bold ">
+                {totalCommentsNumber}
+              </div>
+            </div>
+          </Link>
+        </div>
+
+        {/* More */}
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Ellipsis className=" text-gray-500 hover:text-gray-700 hover:cursor-pointer"/>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>
+              <div className="flex items-center text-gray-800 justify-center gap-2 cursor-pointer" onClick={handleCopyLink}>
+                <Link2 className="w-5"/>
+                <h6>Copy link</h6>
+              </div>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
 };
 
-export default PostCard;
+export default PostCard
 
 
 
