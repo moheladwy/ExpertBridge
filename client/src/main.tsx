2defc549712d0f3bc55857fc28bafd8d349b4178
Kilany <y.m.elkilany@gmail.com>
2025-04-19 22:56:34 +0200

    GOD HELP ME

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -37,25 +37,25 @@ const router = createBrowserRouter([
       {
         path: "home",
         element: (
-          <ProtectedRoute>
+          // <ProtectedRoute>
             <HomePage />
-          </ProtectedRoute>
+          // </ProtectedRoute>
         ),
       },
       {
         path: "posts/:postId",
         element: (
-          <ProtectedRoute>
+          // <ProtectedRoute>
             <PostFromUrlPage />
-          </ProtectedRoute>
+          // </ProtectedRoute>
         ),
       },
       {
         path: "feed/:postId",
         element: (
-          <ProtectedRoute>
+          // <ProtectedRoute>
             <PostFromFeedPage />
-          </ProtectedRoute>
+          // </ProtectedRoute>
         ),
       },
     ],
