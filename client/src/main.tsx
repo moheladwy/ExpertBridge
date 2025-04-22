6938277be4f64e965388a08f2aecf9f01c8db8d3
moheladwy <mohamed.h.eladwy@gmail.com>
2025-04-22 22:48:42 +0200

    Refactor PostCard and NavBar components; add MyProfilePage and UserProfilePage
    - Updated PostCard component for improved readability and structure.
    - Enhanced NavBar with better user profile handling and search functionality.
    - Introduced MyProfilePage for displaying user profile information.
    - Created UserProfilePage to fetch and display other users' profiles.
    - Added necessary API endpoints and hooks for user data retrieval.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,123 +1,133 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './index.css'
-import NotFoundError from './views/components/common/ui/NotFoundError.tsx'
-import LandingPage from './views/pages/landing/LandingPage.tsx'
-import { store } from './app/store.ts'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import NotFoundError from "./views/components/common/ui/NotFoundError.tsx";
+import LandingPage from "./views/pages/landing/LandingPage.tsx";
+import { store } from "./app/store.ts";
 
 import { Provider as ReduxProvider } from "react-redux";
-import SignUpPage from './views/pages/auth/SignUpPage.tsx'
-import LoginPage from './views/pages/auth/LoginPage.tsx'
-import App from './App.tsx'
-import ProtectedRoute from './routes/ProtectedRoute.tsx'
-import PublicRoute from './routes/PublicRoute.tsx'
-import HomePage from './views/pages/feed/HomePage.tsx'
-import Interests from './views/pages/onboarding/Interests.tsx'
-import PostFromFeedPage from './views/pages/posts/PostFromFeedPage.tsx'
-import EmailVerificationPage from './views/pages/auth/EmailVerificationPage.tsx'
+import SignUpPage from "./views/pages/auth/SignUpPage.tsx";
+import LoginPage from "./views/pages/auth/LoginPage.tsx";
+import App from "./App.tsx";
+import ProtectedRoute from "./routes/ProtectedRoute.tsx";
+import PublicRoute from "./routes/PublicRoute.tsx";
+import HomePage from "./views/pages/feed/HomePage.tsx";
+import Interests from "./views/pages/onboarding/Interests.tsx";
+import PostFromFeedPage from "./views/pages/posts/PostFromFeedPage.tsx";
+import EmailVerificationPage from "./views/pages/auth/EmailVerificationPage.tsx";
+import MyProfilePage from "./views/pages/profile/MyProfilePage.tsx";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-import { createTheme, StyledEngineProvider, ThemeProvider } from '@mui/material/styles';
-import PostFromUrlPage from './views/pages/posts/PostFromUrlPage.tsx'
-
+import {
+	createTheme,
+	StyledEngineProvider,
+	ThemeProvider,
+} from "@mui/material/styles";
+import PostFromUrlPage from "./views/pages/posts/PostFromUrlPage.tsx";
+import UserProfilePage from "./views/pages/profile/UserProfilePage.tsx";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />, // Ensures NavBar is always present
-    children: [
-      {
-        index: true,
-        element: <LandingPage />
-      },
-      {
-        path: "home",
-        element: (
-          // <ProtectedRoute>
-            <HomePage />
-          // </ProtectedRoute>
-        ),
-      },
-      {
-        path: "posts/:postId",
-        element: (
-          // <ProtectedRoute>
-            <PostFromUrlPage />
-          // </ProtectedRoute>
-        ),
-      },
-      {
-        path: "feed/:postId",
-        element: (
-          // <ProtectedRoute>
-            <PostFromFeedPage />
-          // </ProtectedRoute>
-        ),
-      },
-    ],
-  },
-  {
-    path: "login",
-    element: (
-      <LoginPage />
-    ),
-  },
-  {
-    path: "signup",
-    element: (
-      <SignUpPage />
-    ),
-  },
-  {
-    path: "verify-email",
-    element: (
-      <EmailVerificationPage />
-    ),
-  },
-  {
-    path: "interests",
-    element: (
-      <ProtectedRoute>
-        <Interests />
-      </ProtectedRoute>
-    ),
-  },
-  { path: "*", element: <NotFoundError /> }, // Catch-all 404
+	{
+		path: "/",
+		element: <App />, // Ensures NavBar is always present
+		children: [
+			{
+				index: true,
+				element: <LandingPage />,
+			},
+			{
+				path: "home",
+				element: (
+					// <ProtectedRoute>
+					<HomePage />
+					// </ProtectedRoute>
+				),
+			},
+			{
+				path: "posts/:postId",
+				element: (
+					// <ProtectedRoute>
+					<PostFromUrlPage />
+					// </ProtectedRoute>
+				),
+			},
+			{
+				path: "feed/:postId",
+				element: (
+					// <ProtectedRoute>
+					<PostFromFeedPage />
+					// </ProtectedRoute>
+				),
+			},
+			{
+				path: "profile",
+				element: (
+					// <ProtectedRoute>
+					<MyProfilePage />
+					// </ProtectedRoute>
+				),
+			},
+			{
+				path: "profile/:userId",
+				element: <UserProfilePage />,
+			},
+		],
+	},
+	{
+		path: "login",
+		element: <LoginPage />,
+	},
+	{
+		path: "signup",
+		element: <SignUpPage />,
+	},
+	{
+		path: "verify-email",
+		element: <EmailVerificationPage />,
+	},
+	{
+		path: "interests",
+		element: (
+			<ProtectedRoute>
+				<Interests />
+			</ProtectedRoute>
+		),
+	},
+	{ path: "*", element: <NotFoundError /> }, // Catch-all 404
 ]);
 
-
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement!);
 
 const theme = createTheme({
-  cssVariables: true,
-  components: {
-    MuiPopover: {
-      defaultProps: {
-        container: rootElement,
-      },
-    },
-    MuiPopper: {
-      defaultProps: {
-        container: rootElement,
-      },
-    },
-  },
+	cssVariables: true,
+	components: {
+		MuiPopover: {
+			defaultProps: {
+				container: rootElement,
+			},
+		},
+		MuiPopper: {
+			defaultProps: {
+				container: rootElement,
+			},
+		},
+	},
 });
 
 root.render(
-  <StrictMode>
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        <ReduxProvider store={store} >
-          <RouterProvider router={router} />
-        </ReduxProvider>
-      </ThemeProvider>
-    </StyledEngineProvider>
-  </StrictMode>
+	<StrictMode>
+		<StyledEngineProvider injectFirst>
+			<ThemeProvider theme={theme}>
+				<ReduxProvider store={store}>
+					<RouterProvider router={router} />
+				</ReduxProvider>
+			</ThemeProvider>
+		</StyledEngineProvider>
+	</StrictMode>
 );
