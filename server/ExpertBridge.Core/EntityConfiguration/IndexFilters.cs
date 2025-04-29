b6c4487a617ffa7a52af33d3eecb98ea670425af
Kilany <y.m.elkilany@gmail.com>
2025-04-29 20:49:10 +0300

    Move files

--- server/ExpertBridge.Core/EntityConfiguration/IndexFilters.cs
+++ server/ExpertBridge.Core/EntityConfiguration/IndexFilters.cs
@@ -1,7 +1,6 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
+﻿
 
-namespace ExpertBridge.Api.Data.EntityConfiguration
+namespace ExpertBridge.Core.Entities
 {
     public static class IndexFilters
     {
