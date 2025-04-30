2033a8d729eca6f4d7c5647622b5d88048185ebd
Kilany <y.m.elkilany@gmail.com>
2025-04-30 16:12:37 +0300

    [api/core] Fix: TaggingService had a major issue with it's queries

--- server/ExpertBridge.Api/Services/TaggingService.cs
+++ server/ExpertBridge.Api/Services/TaggingService.cs
@@ -5,7 +5,8 @@ using System.Threading.Channels;
 using ExpertBridge.Api.Models.IPC;
 using ExpertBridge.Api.Responses;
 using ExpertBridge.Core.Entities.ManyToManyRelationships.PostTags;
-using ExpertBridge.Core.Entities.ManyToManyRelationships.ProfileTags;
+using ExpertBridge.Core.Entities.ManyToManyRelationships.UserInterests;
+using ExpertBridge.Core.Entities.Posts;
 using ExpertBridge.Core.Entities.Tags;
 using ExpertBridge.Data.DatabaseContexts;
 using Microsoft.EntityFrameworkCore;
@@ -33,33 +34,36 @@ namespace ExpertBridge.Api.Services
             ArgumentNullException.ThrowIfNull(postId);
             ArgumentNullException.ThrowIfNull(tags);
 
-            var englishNames = tags.Tags.Select(t => t.EnglishName);
-            var arabicNames = tags.Tags.Select(t => t.ArabicName);
+            var tagList = tags.Tags.ToList();
 
-            var existingTags = _dbContext.Tags
+            var englishNames = tagList.Select(t => t.EnglishName).ToList();
+            var arabicNames = tagList.Select(t => t.ArabicName).ToList();
+
+            var existingTags = await _dbContext.Tags
                 .AsNoTracking()
-                .Where(t => englishNames.Contains(t.EnglishName) || arabicNames.Contains(t.ArabicName));
+                .Where(t => englishNames.Contains(t.EnglishName) || arabicNames.Contains(t.ArabicName))
+                .ToListAsync(); // materialize the query to use it in next calculations.
 
-            var newRawTags = tags.Tags
+            var newRawTags = tagList
                 .Where(t => !existingTags
-                        .Any(et => et.EnglishName == t.EnglishName
-                            || et.ArabicName == t.ArabicName));
-
+                    .Any(et => et.EnglishName == t.EnglishName || et.ArabicName == t.ArabicName))
+                .ToList(); // this LINQ is only client side, thus we can use Any().
 
             var newTags = newRawTags.Select(tag => new Tag
             {
                 EnglishName = tag.EnglishName,
                 ArabicName = tag.ArabicName,
                 Description = tag.Description
-            });
+            }).ToList();
 
             await _dbContext.AddRangeAsync(newTags);
+            await _dbContext.SaveChangesAsync(); // Save new tags to generate their IDs
 
             var post = await _dbContext.Posts.FirstAsync(p => p.Id == postId);
 
             var tagsToAdd = newTags.Concat(existingTags);
 
-            await AddTagsToPostInternalAsync(post.Id, tagsToAdd);
+            await AddTagsToPostInternalAsync(post.Id, tagsToAdd.Select(t => t.Id));
             await AddTagsToUserProfileInternalAsync(authorId, tagsToAdd.Select(t => t.Id));
 
             post.Language = tags.Language;
@@ -75,20 +79,29 @@ namespace ExpertBridge.Api.Services
 
         /// <summary>
         /// This method should not be called from outside because it's just
-        /// a step in a bigger Unit of Work. It does not commit the changes.
+        /// a step in a bigger Unit of Work. It does not commit the changes to DB.
         /// </summary>
-        private async Task AddTagsToPostInternalAsync(string postId, IEnumerable<Tag> tags)
+        private async Task AddTagsToPostInternalAsync(string postId, IEnumerable<string> tagIds)
         {
-            await _dbContext.PostTags.AddRangeAsync(tags.Select(tag => new PostTag
-            {
-                PostId = postId,
-                Tag = tag
-            }));
+            var existingTagIds = await _dbContext.PostTags
+                .Where(pt => pt.PostId == postId && tagIds.Contains(pt.TagId))
+                .Select(pt => pt.TagId)
+                .ToListAsync();
+
+            var newPostTags = tagIds
+                .Where(tagId => !existingTagIds.Contains(tagId))
+                .Select(tagId => new PostTag
+                {
+                    PostId = postId,
+                    TagId = tagId
+                });
+
+            await _dbContext.PostTags.AddRangeAsync(newPostTags);
         }
 
         public async Task AddTagsToPostAsync(string postId, IEnumerable<Tag> tags)
         {
-            await AddTagsToPostInternalAsync(postId, tags);
+            await AddTagsToPostInternalAsync(postId, tags.Select(t => t.Id));
             await _dbContext.SaveChangesAsync();
         }
 
@@ -98,11 +111,20 @@ namespace ExpertBridge.Api.Services
         /// </summary>
         private async Task AddTagsToUserProfileInternalAsync(string profileId, IEnumerable<string> tagIds)
         {
-            await _dbContext.ProfileTags.AddRangeAsync(tagIds.Select(tag => new ProfileTag
-            {
-                ProfileId = profileId,
-                TagId = tag
-            }));
+            var existingTagIds = await _dbContext.UserInterests
+                .Where(ui => ui.ProfileId == profileId && tagIds.Contains(ui.TagId))
+                .Select(pt => pt.TagId)
+                .ToListAsync();
+
+            var newUserInterests = tagIds
+                .Where(tagId => !existingTagIds.Contains(tagId))
+                .Select(tagId => new UserInterest
+                {
+                    ProfileId= profileId,
+                    TagId = tagId
+                });
+
+            await _dbContext.UserInterests.AddRangeAsync(newUserInterests);
         }
 
         public async Task AddTagsToUserProfileAsync(string profileId, IEnumerable<Tag> tags)
