87bb2a482cc081bc2a5366ace0576101c67380a1
Kilany <y.m.elkilany@gmail.com>
2025-05-02 22:14:26 +0300

    [api/core] Add media upload to comments

--- server/ExpertBridge.Core/Requests/CreateComment/CreateCommentRequest.cs
+++ server/ExpertBridge.Core/Requests/CreateComment/CreateCommentRequest.cs
@@ -7,5 +7,5 @@ public class CreateCommentRequest
     public required string PostId { get; set; }
     public string? ParentCommentId { get; set; }
     public required string Content { get; set; }
-    public List<string>? MediaUrls { get; set; }
+    public List<MediaObjectRequest>? Media { get; set; }
 }
