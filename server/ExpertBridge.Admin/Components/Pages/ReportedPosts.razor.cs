f0fedab1f2a5ee7710f27e8ebeeb27391fd16477
moheladwy <mohamed.h.eladwy@gmail.com>
2025-05-06 06:21:47 +0300

    the db context filters the deleted entities by default, and we need to change that
    Signed-off-by: moheladwy <mohamed.h.eladwy@gmail.com>

---
+++ server/ExpertBridge.Admin/Components/Pages/ReportedPosts.razor.cs
@@ -0,0 +1,38 @@
+using ExpertBridge.Core.Entities.Posts;
+using ExpertBridge.Data.DatabaseContexts;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpertBridge.Admin.Components.Pages;
+
+public sealed partial class ReportedPosts : ComponentBase
+{
+    private readonly ExpertBridgeDbContext _dbContext;
+    private List<Post>? reportedPosts;
+
+    public ReportedPosts(ExpertBridgeDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        reportedPosts = [];
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        reportedPosts = await GetReportedPosts();
+        await base.OnInitializedAsync();
+    }
+
+    private async Task<List<Post>> GetReportedPosts()
+    {
+        var posts = await _dbContext.Posts
+            .AsNoTracking()
+            .Where(p => p.IsDeleted)
+            .Include(p => p.Author)
+            .Include(p => p.Comments)
+            .Include(p => p.Votes)
+            .Include(p=> p.Medias)
+            .ToListAsync();
+
+        return posts;
+    }
+}
