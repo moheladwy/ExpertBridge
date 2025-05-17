9c497d9504248886cc01103f1d5587482c24d3bf
moheladwy <mohamed.h.eladwy@gmail.com>
2025-05-17 21:41:57 +0300

    Add LastModified field to PostResponse and PostQueries
    This updates the PostResponse and PostQueries to include the LastModified field. It ensures the response now provides information about the most recent modification date of a post.
    
    Signed-off-by: moheladwy <mohamed.h.eladwy@gmail.com>

--- server/ExpertBridge.Core/Queries/PostQueries.cs
+++ server/ExpertBridge.Core/Queries/PostQueries.cs
@@ -51,6 +51,7 @@ namespace ExpertBridge.Core.Queries
                 Content = p.Content,
                 Author = p.Author.SelectAuthorResponseFromProfile(),
                 CreatedAt = p.CreatedAt.Value,
+                LastModified = p.LastModified.Value,
                 Id = p.Id,
                 Upvotes = p.Votes.Count(v => v.IsUpvote),
                 Downvotes = p.Votes.Count(v => !v.IsUpvote),
