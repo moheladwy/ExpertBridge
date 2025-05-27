a711c721e406850c87cb2bcb6a90301beeb35e02
moheladwy <mohamed.h.eladwy@gmail.com>
2025-05-27 15:25:19 +0300

    Moved the UpdateProfileRequest to ExpertBridge.Core/Requests
    Moved the dto request to the right place

--- server/ExpertBridge.Api/Controllers/ProfilesController.cs
+++ server/ExpertBridge.Api/Controllers/ProfilesController.cs
@@ -166,12 +166,3 @@ public class ProfilesController : ControllerBase
     }
 }
 
-public class UpdateProfileRequest
-{
-    public string? JobTitle { get; set; }
-    public string? Bio { get; set; }
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
-    public string? Username { get; set; }
-    public string? PhoneNumber { get; set; }
-}
