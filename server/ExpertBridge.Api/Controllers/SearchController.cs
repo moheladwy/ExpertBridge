ee22638d936ab6c8917a0acaab52524a8005e408
moheladwy <mohamed.h.eladwy@gmail.com>
2025-06-06 23:13:48 +0300

    implemented the search posts endpoint
    using the vector db and cosine destance I implemented the search
    endpoint.

--- server/ExpertBridge.Api/Controllers/SearchController.cs
+++ server/ExpertBridge.Api/Controllers/SearchController.cs
@@ -1,7 +1,10 @@
+using ExpertBridge.Api.EmbeddingService;
 using ExpertBridge.Core.Responses;
 using ExpertBridge.Data.DatabaseContexts;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pgvector.EntityFrameworkCore;
 
 namespace ExpertBridge.Api.Controllers;
 
@@ -11,15 +14,42 @@ namespace ExpertBridge.Api.Controllers;
 public class SearchController : ControllerBase
 {
     private readonly ExpertBridgeDbContext _dbContext;
+    private readonly OllamaEmbeddingService _embeddingService;
 
-    public SearchController(ExpertBridgeDbContext dbContext)
+
+    public SearchController(
+            ExpertBridgeDbContext dbContext,
+            OllamaEmbeddingService embeddingService)
     {
         _dbContext = dbContext;
+        _embeddingService = embeddingService;
     }
 
-    [HttpGet("posts?query={query}")]
-    public async Task<List<PostResponse>> SearchPosts([FromQuery] string query)
+    // TODO: Test this endpoint before deploying.
+    [HttpGet("posts")]
+    public async Task<List<PostResponse>> SearchPosts(
+            [FromQuery] string query,
+            [FromQuery] int? limit = null,
+            CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException("Search functionality is not yet implemented.");
+        ArgumentNullException.ThrowIfNullOrEmpty(query, nameof(query));
+        var embedding = await _embeddingService.GenerateEmbedding(query);
+        if (embedding == null)
+        {
+            throw new InvalidOperationException("Failed to generate embedding for the query.");
+        }
+        var posts = await _dbContext.Posts
+            .AsNoTracking()
+            .Where(p => p.Embedding != null)
+            .OrderBy(p => p.Embedding.CosineDistance(embedding))
+            .Take(limit ?? 100)
+            .Select(p => new PostResponse
+            {
+                Id = p.Id,
+                Title = p.Title,
+                Content = p.Content,
+            })
+            .ToListAsync(cancellationToken);
+        return posts ?? [];
     }
 }
