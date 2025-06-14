f7c38958b0a2501cada213d2913c623b7dc161a0
moheladwy <mohamed.h.eladwy@gmail.com>
2025-06-15 00:36:15 +0300

    create a sql script for the Add Index On Post Embeddings Migration

---
+++ server/ExpertBridge.Data/Migrations/Scripts/AddIndexOnPostEmbeddings.sql
@@ -0,0 +1,8 @@
+﻿START TRANSACTION;
+CREATE INDEX "IX_Posts_Embedding" ON "Posts" USING hnsw ("Embedding" vector_cosine_ops);
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20250517122905_AddIndexOnPostEmbedding', '9.0.4');
+
+COMMIT;
+
