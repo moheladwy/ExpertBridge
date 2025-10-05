85390337b7da214a22b1996f6354c16e6e7954d1
moheladwy <mohamed.h.eladwy@gmail.com>
2025-10-05 18:23:36 +0300

    refactor: Remove old database and sync services; implement new routing structure

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,213 +1,47 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import NotFoundError from "./views/components/common/ui/NotFoundError.tsx";
-import LandingPage from "./views/pages/landing/LandingPage.tsx";
 import { store } from "./app/store.ts";
-
+import { router } from "./routes.tsx";
 import { Provider as ReduxProvider } from "react-redux";
-import SignUpPage from "./views/pages/auth/SignUpPage.tsx";
-import LoginPage from "./views/pages/auth/LoginPage.tsx";
-import App from "./App.tsx";
-import ProtectedRoute from "./routes/ProtectedRoute.tsx";
-import HomePage from "./views/pages/feed/HomePage.tsx";
-import Interests from "./views/pages/onboarding/Interests.tsx";
-import PostFromFeedPage from "./views/pages/posts/PostFromFeedPage.tsx";
-import EmailVerificationPage from "./views/pages/auth/EmailVerificationPage.tsx";
-import MyProfilePage from "./views/pages/profile/MyProfilePage.tsx";
-
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-
 import {
-  createTheme,
-  StyledEngineProvider,
-  ThemeProvider,
+	createTheme,
+	StyledEngineProvider,
+	ThemeProvider,
 } from "@mui/material/styles";
-import PostFromUrlPage from "./views/pages/posts/PostFromUrlPage.tsx";
-import UserProfilePage from "./views/pages/profile/UserProfilePage.tsx";
-import Notifications from "./views/pages/notifications/Notifications.tsx";
-import SearchPosts from "./views/pages/search/SearchPosts.tsx";
-import SearchUsers from "./views/pages/search/SearchUsers.tsx";
-import JobPostingsFeed from "./views/components/common/jobPostings/JobPostingsFeed.tsx";
-import JobPostingFromFeedPage from "./views/pages/jobPostings/JobPostingFromFeedPage.tsx";
-import JobPostingFromUrlPage from "./views/pages/jobPostings/JobPostingFromUrlPage.tsx";
-import PrivacyPolicy from "./views/pages/landing/PrivacyPolicy.tsx";
-import JobApplicationsPage from "./views/pages/jobPostings/JobApplicationsPage.tsx";
-import { JobOffersDashboardPage } from "./views/pages/jobs/JobOffersDashboardPage.tsx";
-import { MyJobsPage } from "./views/pages/jobs/MyJobsPage.tsx";
-import { JobDetailsPage } from "./views/pages/jobs/JobDetailsPage.tsx";
-import SearchJobPosts from "./views/pages/search/SearchJobPosts.tsx";
-import AboutUsPage from "./views/pages/landing/AboutUsePage.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />, // Ensures NavBar is always present
-    children: [
-      {
-        index: true,
-        element: <LandingPage />,
-      },
-      {
-        path: "home",
-        element: (
-          // <ProtectedRoute>
-          <HomePage />
-          // </ProtectedRoute>
-        ),
-      },
-      {
-        path: "posts/:postId",
-        element: (
-          // <ProtectedRoute>
-          <PostFromUrlPage />
-          // </ProtectedRoute>
-        ),
-      },
-      {
-        path: "feed/:postId",
-        element: (
-          // <ProtectedRoute>
-          <PostFromFeedPage />
-          // </ProtectedRoute>
-        ),
-      },
-      {
-        path: "jobs",
-        element: <JobPostingsFeed />,
-      },
-      {
-        path: "jobs/:jobPostingId",
-        element: <JobPostingFromFeedPage />,
-      },
-      {
-        path: "job/:jobPostingId",
-        element: <JobPostingFromUrlPage />,
-      },
-      {
-        path: "job/:jobPostingId/applications",
-        element: <JobApplicationsPage />,
-      },
-      {
-        path: "profile",
-        element: (
-          <ProtectedRoute>
-            <MyProfilePage />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "profile/:userId",
-        element: <UserProfilePage />,
-      },
-      {
-        path: "search/p",
-        element: <SearchPosts />,
-      },
-      {
-        path: "search/u",
-        element: <SearchUsers />,
-      },
-      {
-        path: "search/jobs",
-        element: <SearchJobPosts />,
-      },
-      {
-        path: "notifications",
-        element: (
-          <ProtectedRoute>
-            <Notifications />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "offers",
-        element: (
-          <ProtectedRoute>
-            <JobOffersDashboardPage />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "my-jobs",
-        element: (
-          <ProtectedRoute>
-            <MyJobsPage />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "my-jobs/:jobId",
-        element: (
-          <ProtectedRoute>
-            <JobDetailsPage />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "privacy-policy",
-        element: <PrivacyPolicy />,
-      },
-      {
-        path: "AboutUs",
-        element: <AboutUsPage />,
-      },
-    ],
-  },
-  {
-    path: "login",
-    element: <LoginPage />,
-  },
-  {
-    path: "signup",
-    element: <SignUpPage />,
-  },
-  {
-    path: "verify-email",
-    element: <EmailVerificationPage />,
-  },
-  {
-    path: "onboarding",
-    element: (
-      <ProtectedRoute>
-        <Interests />
-      </ProtectedRoute>
-    ),
-  },
-  { path: "*", element: <NotFoundError /> }, // Catch-all 404
-]);
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement!);
 
 const theme = createTheme({
-  cssVariables: true,
-  components: {
-    MuiPopover: {
-      defaultProps: {
-        container: rootElement,
-      },
-    },
-    MuiPopper: {
-      defaultProps: {
-        container: rootElement,
-      },
-    },
-  },
+	cssVariables: true,
+	components: {
+		MuiPopover: {
+			defaultProps: {
+				container: rootElement,
+			},
+		},
+		MuiPopper: {
+			defaultProps: {
+				container: rootElement,
+			},
+		},
+	},
 });
 
 root.render(
-  <StrictMode>
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        <ReduxProvider store={store}>
-          <RouterProvider router={router} />
-        </ReduxProvider>
-      </ThemeProvider>
-    </StyledEngineProvider>
-  </StrictMode>,
+	<StrictMode>
+		<StyledEngineProvider injectFirst>
+			<ThemeProvider theme={theme}>
+				<ReduxProvider store={store}>
+					<RouterProvider router={router} />
+				</ReduxProvider>
+			</ThemeProvider>
+		</StyledEngineProvider>
+	</StrictMode>
 );
