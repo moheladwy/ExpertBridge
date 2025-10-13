014cd1649a7285f8ff2d0a21aa73ecb7e73efe6e
moheladwy <mohamed.h.eladwy@gmail.com>
2025-10-13 21:07:55 +0300

    feat: Add `PostTaggingConsumer` for tag generation and processing
    - Introduced `PostTaggingConsumer` to process `TagPostMessage` messages.
    - Integrated `GroqPostTaggingService` for tag generation based on post content and metadata.
    - Added tag assignment logic with `TaggingService` to handle posts atomically.
    - Enhanced error logging for debugging and processing failures.
    
    Signed-off-by: moheladwy <mohamed.h.eladwy@gmail.com>

---
+++ server/ExpertBridge.Worker/Consumers/PostTaggingConsumer.cs
@@ -0,0 +1,48 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using ExpertBridge.Application.DomainServices;
+using ExpertBridge.Application.Services;
+using ExpertBridge.Contract.Messages;
+using MassTransit;
+
+namespace ExpertBridge.Worker.Consumers;
+
+internal sealed class PostTaggingConsumer : IConsumer<TagPostMessage>
+{
+    private readonly ILogger<PostTaggingConsumer> _logger;
+    private readonly GroqPostTaggingService _groqPostTaggingService;
+    private readonly TaggingService _taggingService;
+
+    public PostTaggingConsumer(
+        ILogger<PostTaggingConsumer> logger,
+        GroqPostTaggingService groqPostTaggingService,
+        TaggingService taggingService)
+    {
+        _logger = logger;
+        _groqPostTaggingService = groqPostTaggingService;
+        _taggingService = taggingService;
+    }
+
+    public async Task Consume(ConsumeContext<TagPostMessage> context)
+    {
+        try
+        {
+            var post = context.Message;
+            var tags = await _groqPostTaggingService
+                .GeneratePostTagsAsync(post.Title, post.Content, new List<string>());
+
+            // Atomic Operation.
+            await _taggingService.AddRawTagsToPostAsync(
+                post.PostId,
+                post.AuthorId,
+                post.IsJobPosting,
+                tags,
+                context.CancellationToken);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"An error occurred while processing post with id={context.Message.PostId}.");
+        }
+    }
+}
