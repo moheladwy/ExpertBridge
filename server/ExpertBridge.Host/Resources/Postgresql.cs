eb8215f846692e4cfd27c273d2dbe3dc065266e9
moheladwy <mohamed.h.eladwy@gmail.com>
2025-10-19 22:43:41 +0300

    feat: Modularize distributed application resource configuration
    - Introduced extension methods for setting up RabbitMQ, Redis, Seq, Postgresql, and Ollama resources as reusable components.
    - Updated Program.cs to utilize the modular resource configuration methods for cleaner and more maintainable code structure.
    - Enhanced appsettings.Development.json with Postgresql configuration parameters.
    
    Signed-off-by: moheladwy <mohamed.h.eladwy@gmail.com>

---
+++ server/ExpertBridge.Host/Resources/Postgresql.cs
@@ -0,0 +1,50 @@
+// Licensed to the.NET Foundation under one or more agreements.
+// The.NET Foundation licenses this file to you under the MIT license.
+
+namespace ExpertBridge.Host.Resources;
+
+/// <summary>
+/// Provides extension methods for configuring Postgresql resources in a distributed application setup.
+/// </summary>
+internal static class Postgresql
+{
+    /// <summary>
+    /// Configures and provides a resource builder for a Postgres server resource, integrated with the application
+    /// builder. This includes database and PgAdmin setup with predefined configurations for container name, lifetime,
+    /// data volume, environment variables, and OTLP exporter.
+    /// </summary>
+    /// <param name="builder">The distributed application builder used to configure and manage distributed resources.</param>
+    /// <returns>A resource builder for the Postgres server resource, ready for deployment and integration within the application.</returns>
+    public static IResourceBuilder<PostgresServerResource> GetPostgresqlResource(this IDistributedApplicationBuilder builder)
+    {
+        var postgresqlUserName = builder
+            .AddParameterFromConfiguration("Postgres-Username", "Postgres:Username");
+        var postgresqlPassword = builder
+            .AddParameterFromConfiguration("Postgres-Password", "Postgres:Password");
+
+        var postgresql = builder
+                .AddPostgres("Postgres", postgresqlUserName, postgresqlPassword, 5432)
+                .WithImage("pgvector/pgvector", "pg17")
+                .WithContainerName("expertbridge-postgres")
+                .WithDataVolume("expertbridge-postgres-data")
+                .WithLifetime(ContainerLifetime.Persistent)
+                .WithPgAdmin(cfg =>
+                {
+                    cfg
+                        .WithContainerName("expertbridge-pgadmin")
+                        .WithHostPort(5050)
+                        .WithLifetime(ContainerLifetime.Persistent)
+                        .WithEnvironment("PGADMIN_DEFAULT_EMAIL", "admin@admin.com")
+                        .WithEnvironment("PGADMIN_DEFAULT_PASSWORD", "admin@admin")
+                        .WithEnvironment("PGADMIN_CONFIG_FORCE_SSL", "True")
+                        .WithEnvironment("PGADMIN_CONFIG_ENHANCED_COOKIE_PROTECTION", "False")
+                        .WithEnvironment("PGADMIN_CONFIG_WTF_CSRF_CHECK_DEFAULT", "False")
+                        .WithEnvironment("PGADMIN_CONFIG_WTF_CSRF_ENABLED", "False")
+                        .WithExternalHttpEndpoints();
+                })
+                .WithOtlpExporter()
+            ;
+
+        return postgresql;
+    }
+}
---
+++ server/ExpertBridge.Host/Resources/Redis.cs
@@ -0,0 +1,22 @@
+// Licensed to the.NET Foundation under one or more agreements.
+// The.NET Foundation licenses this file to you under the MIT license.
+
+namespace ExpertBridge.Host.Resources;
+
+internal static class Redis
+{
+    public static IResourceBuilder<RedisResource> GetRedisResource(this IDistributedApplicationBuilder builder)
+    {
+        var redis = builder
+            .AddRedis("Redis", 6379)
+            .WithImage("redis", "alpine")
+            .WithContainerName("expertbridge-redis")
+            .WithDataVolume("expertbridge-redis-data")
+            .WithPersistence(TimeSpan.FromMinutes(5))
+            .WithLifetime(ContainerLifetime.Persistent)
+            .WithOtlpExporter()
+            .PublishAsConnectionString();
+
+        return redis;
+    }
+}
