06e25c7b2ac852961254d41ed62220d368e17e69
moheladwy <mohamed.h.eladwy@gmail.com>
2025-10-26 23:53:31 +0300

    feat: Enable Ollama resource and integrate it with Admin and Worker projects
    - Activated Ollama resource and added references within Admin and Worker projects.
    - Improved system initialization and dependency handling by including Ollama in the resource setup.
    - Restored and updated Admin and Worker project configurations for comprehensive support.
    
    Signed-off-by: moheladwy <mohamed.h.eladwy@gmail.com>

--- server/ExpertBridge.Host/Program.cs
+++ server/ExpertBridge.Host/Program.cs
@@ -4,7 +4,7 @@ using Projects;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var rabbitMq = builder.GetRabbitMqResource();
-// var ollama = builder.GetOllamaResource();
+var ollama = builder.GetOllamaResource();
 var redis = builder.GetRedisResource();
 var seq = builder.GetSeqResource();
 var postgresql = builder.GetPostgresqlResource();
@@ -14,36 +14,38 @@ builder.AddProject<ExpertBridge_Api>("ExpertBridgeApi")
     .WithReference(seq)
     .WithReference(postgresql)
     .WithReference(rabbitMq)
+    .WithReference(ollama)
     .WaitFor(rabbitMq)
     .WaitFor(redis)
     .WaitFor(seq)
     .WaitFor(rabbitMq)
+    .WaitFor(ollama)
     .WithOtlpExporter()
     .WithExternalHttpEndpoints();
 
-// builder.AddProject<ExpertBridge_Admin>("ExpertBridgeAdmin")
-//     .WithReference(redis)
-//     .WithReference(seq)
-//     .WithReference(postgresql)
-//     .WithReference(rabbitMq)
-//     .WaitFor(rabbitMq)
-//     .WaitFor(seq)
-//     .WaitFor(redis)
-//     .WaitFor(rabbitMq)
-//     .WithOtlpExporter()
-//     .WithExternalHttpEndpoints();
-//
-// builder.AddProject<ExpertBridge_Worker>("ExpertBridgeWorker")
-//     .WithReference(redis)
-//     .WithReference(rabbitMq)
-//     .WithReference(seq)
-//     .WithReference(ollama)
-//     .WithReference(postgresql)
-//     .WaitFor(rabbitMq)
-//     .WaitFor(redis)
-//     .WaitFor(seq)
-//     .WaitFor(ollama)
-//     .WaitFor(postgresql)
-//     .WithOtlpExporter();
+builder.AddProject<ExpertBridge_Admin>("ExpertBridgeAdmin")
+    .WithReference(redis)
+    .WithReference(seq)
+    .WithReference(postgresql)
+    .WithReference(rabbitMq)
+    .WaitFor(rabbitMq)
+    .WaitFor(seq)
+    .WaitFor(redis)
+    .WaitFor(rabbitMq)
+    .WithOtlpExporter()
+    .WithExternalHttpEndpoints();
+
+builder.AddProject<ExpertBridge_Worker>("ExpertBridgeWorker")
+    .WithReference(redis)
+    .WithReference(rabbitMq)
+    .WithReference(seq)
+    .WithReference(ollama)
+    .WithReference(postgresql)
+    .WaitFor(rabbitMq)
+    .WaitFor(redis)
+    .WaitFor(seq)
+    .WaitFor(ollama)
+    .WaitFor(postgresql)
+    .WithOtlpExporter();
 
 await builder.Build().RunAsync();
